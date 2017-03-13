e610ebdcfd77c86191bee09029ccd95a22ee9a74
fesenkod <fesenko.dmytro.finance@gmail.com>
2017-03-13 22:11:55 +0200

    Add all necessary features
    TODO: code refactoring

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -6,26 +6,23 @@ employeesRegisterApp.controller("loginCtrl", function ($scope, $http) {
 
   $scope.signOut();
 
-  $http.get("users.json").then(function(response) {
-        $scope.users = response.data;
-        console.log($scope.users);
-    });
-
   $scope.isNotExist = false;
 
   $scope.sign = function (login, passw) {
     $scope.isNotExist = false;
-    for (var i = 0; i < $scope.users["Users"].length; i++) {
-      if ($scope.users["Users"][i].login == login && $scope.users["Users"][i].password == passw) {
-        localStorage.setItem('currentUser', JSON.stringify($scope.users["Users"][i]));
-        location.href = "#table";
-        return;
-      };
-    };
-    $scope.isNotExist = true;
+    $http.get("/Users?login=" + login + "&password=" + sha256(passw)).then(function(response) {
+          $scope.user = response.data[0];
+          if ($scope.user && $scope.user.id) {
+              localStorage.setItem('currentUser', JSON.stringify($scope.user));
+              location.href = "#table";
+              return;
+            };
+          $scope.isNotExist = true;
+      });
   };
 
 });
+
 
 employeesRegisterApp.controller("tableCtrl", function ($scope, $http) {
   var initTest = function () {
@@ -36,41 +33,146 @@ employeesRegisterApp.controller("tableCtrl", function ($scope, $http) {
   initTest();
 
   $scope.employList = [];
+  $scope.selectedCheckboxes = [];
+  $scope.showHelp = false;
   $scope.id;
+  $scope.sortType = '';
+  $scope.sortReverse = '';
+  $scope.search = {};
+  $scope.isFilterSet = false;
 
-  $http.get("employees.json").then(function(response) {
-    for (var i = 0; i < response.data["Employees"].length; i++) {
-      $scope.employList.push(response.data["Employees"][i]);
+  $scope.setReverse = function () {
+    if ($scope.sortReverse === '') {
+      $scope.sortReverse = false;
+    }
+    else {
+      $scope.sortReverse = !$scope.sortReverse;
+    };
+  };
+
+  $scope.applyFilter = function(name, surname, position) {
+    $scope.addOrEditForm.$setPristine();
+    $scope.search['firstName'] = name;
+    $scope.search['secondName'] = surname;
+    $scope.search['position'] = position;
+    if ($scope.search['firstName'] || $scope.search['secondName'] || $scope.search['position']) {
+      $scope.isFilterSet = true;
+    }
+    else {
+      $scope.isFilterSet = false;
+    };  
+  };
+
+  $scope.clearFilter = function () {
+    $scope.search = {};
+    $scope.isFilterSet = false;
+  };
+
+  $http.get("/Employees").then(function(response) {
+    for (var i = 0; i < response.data.length; i++) {
+      $scope.employList.push(response.data[i]);
       };
     console.log($scope.employList);
     });
 
   $scope.getId = function() {
-    $scope.id = $scope.employList[$scope.employList.length-1].Id;
+    // TODO: change function to communicate with server rather than operates on client's employees array
+    $scope.id = $scope.employList.length > 0 ? $scope.employList[$scope.employList.length-1]['id'] : 0;
   };
 
-  $scope.showHelp = false;
+  $scope.clearForm = function () {
+    $scope.addOrEditForm.$setPristine();
+    $scope.firstName = "";
+    $scope.secondName = "";
+    $scope.position = "";
+  }
 
-  $scope.save = function (name, surname, position) {
+  $scope.saveOrEdit = function (name, surname, position) {
+    $scope.showHelp = false;
     if (!name || !surname || !position) {
       $scope.showHelp = true;
       return;
     };
+    // for supporting edit
+    if ($scope.selectedCheckboxes.length == 1) {
+      var newObj = {
+        "id": $scope.selectedCheckboxes[0],
+        "firstName": name,
+        "secondName": surname,
+        "position": position
+      };
+      $http.put("/Employees/" + $scope.selectedCheckboxes[0], newObj).then(function (response) {
+        for (var i = 0; i < $scope.employList.length; i++) {
+          if ($scope.employList[i].id == response.data.id) {
+            $scope.employList.splice(i, 1, response.data);
+            console.log(response.data);
+          };
+        };
+      });
+      $scope.selectedCheckboxes = [];
+      $scope.clearForm();
+      return;
+    };
+    // for supporting adding new employees
     $scope.getId();
     var newObj = {
-      "Id": $scope.id + 1,
+      "id": $scope.id + 1,
       "firstName": name,
       "secondName": surname,
       "position": position
     };
-    $http({method: 'POST',
-    url: "/employees",
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    data: newObj}).then(function (response) {
-      employList.push(response.data);
+    $http.post("/Employees", newObj).then(function (response) {
+      $scope.employList.push(response.data);
     });
+    $scope.clearForm();
   };
+
+
+  $scope.isChecked = function (empl_id) {
+    var index = $scope.selectedCheckboxes.indexOf(empl_id);
+    if (index == -1) {
+          $scope.selectedCheckboxes.push(empl_id);
+      }
+    else {
+          $scope.selectedCheckboxes.splice(index, 1);
+    };
+    console.log($scope.selectedCheckboxes);
+    if ($scope.selectedCheckboxes.length == 1) {
+      for (var i = 0; i < $scope.employList.length; i++) {
+        if ($scope.employList[i].id == $scope.selectedCheckboxes[0]) {
+          $scope.firstName = $scope.employList[i].firstName;
+          $scope.secondName = $scope.employList[i].secondName;
+          $scope.position = $scope.employList[i].position;
+          break;
+        };
+      };
+    }
+    else {
+      $scope.firstName = "";
+      $scope.secondName = "";
+      $scope.position = "";
+    };
+  };
+
+
+  $scope.delete = function () {
+    for (var i = 0; i < $scope.selectedCheckboxes.length; i++) {
+      for (var j = 0; j < $scope.employList.length; j++) {
+        if ($scope.employList[j].id == $scope.selectedCheckboxes[i]) {
+          $scope.employList.splice(j, 1);
+          $http.delete("/Employees/" + $scope.selectedCheckboxes[i]).then(function (response) {});
+          break;
+        };
+      };
+      $scope.selectedCheckboxes.splice(i, 1);
+      i--;
+    };
+    $scope.clearForm();
+  };
+
+  // TODO: filter function should delete some values from the selectedCheckboxes array
+
+
+
 
 });
---
+++ readme.txt
@@ -0,0 +1,62 @@
+"Users": [
+  {
+    "id": 1,
+    "name": "testUser1",
+    "login": "testLogin1",
+    "password": "test1"
+  },
+  {
+    "id": 2,
+    "name": "testUser2",
+    "login": "testLogin2",
+    "password": "test12"
+  },
+  {
+    "id": 3,
+    "name": "testUser3",
+    "login": "testLogin3",
+    "password": "test123"
+  },
+  {
+    "id": 4,
+    "name": "testUser4",
+    "login": "testLogin4",
+    "password": "test1234"
+  },
+  {
+    "id": 5,
+    "name": "testUser5",
+    "login": "testLogin5",
+    "password": "test12345"
+  },
+  {
+    "id": 6,
+    "name": "testUser6",
+    "login": "testLogin6",
+    "password": "test12345"
+  },
+  {
+    "id": 7,
+    "name": "testUser7",
+    "login": "testLogin7",
+    "password": "test1234"
+  },
+  {
+    "id": 8,
+    "name": "testUser8",
+    "login": "testLogin8",
+    "password": "test123"
+  },
+  {
+    "id": 9,
+    "name": "testUser9",
+    "login": "testLogin9",
+    "password": "test12"
+  },
+  {
+    "id": 10,
+    "name": "testUser10",
+    "login": "testLogin10",
+    "password": "test1"
+  }
+]
